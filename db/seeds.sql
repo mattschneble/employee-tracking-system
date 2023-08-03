c5a704db228313334b3a8f81c93cfda8b29fa7f8
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-02 17:10:04 -0700

    created SQL files

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Sales Lead', '125000', 1),
+    ('Salesperson', '75000', 1),
+    ('Lead Software Engineer', '175000', 2),
+    ('Software Engineer', '120000', 2),
+    ('Accountant', '125000', 3),
+    ('Lead Attorney', '250000', 4),
+    ('Attorney', '190000', 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ('Indiana', 'Jones' 1),
+    ('Rick', 'Dalton', 2),
+    ('Cliff', 'Booth', 2),
+    ('Optimus', 'Prime', 3),
+    ('Bilbo', 'Baggins', 4),
+    ('Samwell', 'Tarly', 4)
+    ('Petyr', 'Baelish', 5),
+    ('Christian', 'Wolff', 5),
+    ('Leo', 'McGary', 6),
+    ('Samuel', 'Seaborn', 7),
+    ('Josh', 'Lyman', 7);
+
+UPDATE employee SET manager_id = 1 where id = 2 or id = 3;
+UPDATE employee SET manager_id = 4 where id = 5 or id = 6;
+UPDATE employee SET manager_id = 9 where id = 10 or id = 11;
