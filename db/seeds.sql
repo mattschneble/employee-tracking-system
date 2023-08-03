0cfbccce039dc8b942ae51ec12f00dbefbc1e082
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-02 17:21:33 -0700

    updated seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,22 +7,22 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Sales Lead', '125000', 1),
-    ('Salesperson', '75000', 1),
-    ('Lead Software Engineer', '175000', 2),
-    ('Software Engineer', '120000', 2),
-    ('Accountant', '125000', 3),
-    ('Lead Attorney', '250000', 4),
-    ('Attorney', '190000', 4);
+    ('Sales Lead', 125000, 1),
+    ('Salesperson', 75000, 1),
+    ('Lead Software Engineer', 175000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Accountant', 125000, 3),
+    ('Lead Attorney', 250000, 4),
+    ('Attorney', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
-    ('Indiana', 'Jones' 1),
+    ('Indiana', 'Jones', 1),
     ('Rick', 'Dalton', 2),
     ('Cliff', 'Booth', 2),
     ('Optimus', 'Prime', 3),
     ('Bilbo', 'Baggins', 4),
-    ('Samwell', 'Tarly', 4)
+    ('Samwell', 'Tarly', 4),
     ('Petyr', 'Baelish', 5),
     ('Christian', 'Wolff', 5),
     ('Leo', 'McGary', 6),
